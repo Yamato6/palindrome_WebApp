c9a8d7910f6167aac9d7876702a551fed7b48601
Yamato61 <jeshuaabrahamd@gmail.com>
2026-03-11 15:00:58 -0600

    Deleted ParseInt

--- palindrome.js
+++ palindrome.js
@@ -2,18 +2,23 @@
 //Create a function to check if a number is a palindrom
 //example: 12321 is a palindrome, but 12345 is false
 
-function isPalindrome(num) {
-  let og = num;
+function isPalindrome(palindrome) {
+  let og = palindrome;
   let reverse = 0;
 
-  while (num > 0) {
-    let last_digit = num % 10;
+  while (palindrome > 0) {
+    //get the last digit of the number
+    let last_digit = palindrome % 10;
+    //add the last digit to the reverse number
     reverse = reverse * 10 + last_digit;
-    num = parseInt(num / 10);
+    //remove the last digit from the original number
+    palindrome = (palindrome / 10);
+    //remove decimal
+    palindrome = palindrome - (last_digit * 0.1);
   }
-
+  //check if the original number is equal to the reverse number
   return og === reverse;
 }
 
-const result = isPalindrome(1231);
+const result = isPalindrome(12321);
 console.log(result);
